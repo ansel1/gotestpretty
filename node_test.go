ec64e62b99b35962c3f0fc605dd0aed2dc8fe155
Russ Egan <russell.egan@thalesgroup.com>
2024-10-28 19:34:19 -0500

    Fixed package msg pattern matching for an additional case

--- node_test.go
+++ node_test.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPattern(t *testing.T) {
-
-}
-
 func TestPackageMsg(t *testing.T) {
-	testCases := map[string]string{
-		"FAIL\tgitlab.protectv.local/ncryptify/sallyport.git/models\t5.430s\n":                                          "",
-		"\tgitlab.protectv.local/ncryptify/sallyport.git/cmd/sallyport\t\tcoverage: 0.0% of statements\n":               "coverage: 0.0% of statements",
-		"ok  \tgitlab.protectv.local/ncryptify/sallyport.git\t0.946s\tcoverage: 0.0% of statements [no tests to run]\n": "coverage: 0.0% of statements [no tests to run]",
+	// all the test lines are in a single text block.  The test will split this into lines, and compare each line with
+	// the corresponding index in `out`.
+	// To add more test cases, add another line, terminated with a newline, to `in`, and a new expectation to `out`.
+	//
+	// This is one text block to make it easy to copy all the samples into a regex tool to test them all against
+	// the pattern at once.
+
+	in := `FAIL	gitlab.protectv.local/ncryptify/sallyport.git/models	5.430s
+	gitlab.protectv.local/ncryptify/sallyport.git/cmd/sallyport		coverage: 0.0% of statements
+ok  	gitlab.protectv.local/ncryptify/sallyport.git	0.946s	coverage: 0.0% of statements [no tests to run]
+?   	gitlab.protectv.local/ncryptify/minerva.git/cryptocore	[no test files]
+`
+	out := []string{
+		"",
+		"coverage: 0.0% of statements",
+		"coverage: 0.0% of statements [no tests to run]",
+		"[no test files]",
 	}
-	for in, out := range testCases {
+
+	for i, line := range strings.SplitAfter(in, "\n") {
+		if i+1 > len(out) {
+			return
+		}
 		n := &node{lvl: 1}
 
-		n.output(in)
-		assert.Equal(t, out, n.msg)
+		n.output(line)
+		assert.Equal(t, out[i], n.msg, "incorrect output for line %v: %v", i, line)
 	}
 }
